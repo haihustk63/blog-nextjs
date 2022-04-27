46cb0647860d0ad90bb387b8aba1b25777d8aa1d
haihustk63 <haiphamtk13clc@gmail.com>
2022-04-27 09:56:44 +0700

    27.4-4

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,8 +1,8 @@
 import Head from "next/head";
-import Layout from "../../components/layout";
+import Layout from "../../components/Layout.js";
 import { getAllPostIds, getPostData } from "../../lib/posts";
 import utilStyles from "../../styles/utils.module.css";
-import Date from "../../components/date";
+import Date from "../../components/Date.js";
 
 export async function getStaticPaths() {
   const paths = getAllPostIds();
